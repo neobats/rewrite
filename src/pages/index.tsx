dbe027e6d90ff0ba488230bef44bc1db1a5f8929
neobats <batton.guy@gmail.com>
2021-09-07 12:48:05 -0500

    added packages

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import * as React from "react"
+import { Box } from "../components/system/Box"
+
+const IndexPage = () => {
+  return (
+    <Box width="100%">
+      <p>Hello world!</p>
+    </Box>
+  )
+}
+
+export default IndexPage
